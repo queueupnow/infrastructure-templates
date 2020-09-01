58b018c8cde0b2a89d7ae176ce63f755dfdbd99a
Sam Gendler <sgendler@tunein.com>
2020-09-01 12:17:06 -0700

    refer to modules via git urls (#1)

--- security/cross-account-roles/main.tf
+++ security/cross-account-roles/main.tf
@@ -1,7 +1,7 @@
 
 
 module "cross-account-roles" {
-  source = "../../..//modules/security/cross-account-roles"
+  source = "git@github.com:queueupnow/infrastructure-modules.git//security/cross-account-roles?ref=v0.0.1"
 
   aws_account_id = var.aws_account_id
 
--- security/iam-groups/main.tf
+++ security/iam-groups/main.tf
@@ -1,5 +1,5 @@
 module "iam-groups" {
-  source = "../../..//modules/security/iam-groups"
+  source = "git@github.com:queueupnow/infrastructure-modules.git//security/iam-groups?ref=v0.0.1"
 
   aws_account_id                      = var.aws_account_id
   require_mfa                         = var.require_mfa
